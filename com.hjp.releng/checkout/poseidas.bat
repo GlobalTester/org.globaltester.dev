69a06d9642f26fb3c34dd7f1b46588919750074e
Alexander May <alexander.may@hjp-consulting.com>
2015-08-06 15:12:18 +0200

    add checkout script for POSeIDAS

---
+++ com.hjp.releng/checkout/poseidas.bat
@@ -0,0 +1,8 @@
+git clone git@github.com:PersoSim/de.persosim.driver.connector.git
+git clone git@github.com:PersoSim/de.persosim.rcp.git
+git clone git@github.com:PersoSim/de.persosim.simulator.git
+git clone git@github.com:GlobalTester/org.globaltester.cryptoprovider.git
+git clone git@github.com:GlobalTester/org.globaltester.logging.git
+git clone ssh://git@tourmaline.intranet.hjp-consulting.com/com.hjp.poseidas
+git clone ssh://git@tourmaline.intranet.hjp-consulting.com/com.hjp.persosim.simulator.protocols.ca3
+git clone ssh://git@tourmaline.intranet.hjp-consulting.com/com.hjp.persosim.simulator.protocols.ps
