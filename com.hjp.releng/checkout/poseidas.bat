c548386a0437d8e17240654c010b5af195b334e6
Alexander May <alexander.may@hjp-consulting.com>
2015-08-09 22:48:06 +0200

    Some fixes to chjeckout script
    * fix repo paths (remove trailing .git)
    * add o.g.parent in order to allow maven build

--- com.hjp.releng/checkout/poseidas.bat
+++ com.hjp.releng/checkout/poseidas.bat
@@ -1,8 +1,9 @@
-git clone git@github.com:PersoSim/de.persosim.driver.connector.git
-git clone git@github.com:PersoSim/de.persosim.rcp.git
-git clone git@github.com:PersoSim/de.persosim.simulator.git
-git clone git@github.com:GlobalTester/org.globaltester.cryptoprovider.git
-git clone git@github.com:GlobalTester/org.globaltester.logging.git
-git clone ssh://git@git.hjp-consulting.com/com.hjp.poseidas
-git clone ssh://git@git.hjp-consulting.com/com.hjp.persosim.simulator.protocols.ca3
-git clone ssh://git@git.hjp-consulting.com/com.hjp.persosim.simulator.protocols.ps
+git clone git@git.hjp-consulting.com:com.hjp.persosim.simulator.protocols.ca3
+git clone git@git.hjp-consulting.com:com.hjp.persosim.simulator.protocols.ps
+git clone git@git.hjp-consulting.com:com.hjp.poseidas
+git clone git@git.hjp-consulting.com:de.persosim.driver.connector
+git clone git@git.hjp-consulting.com:de.persosim.rcp
+git clone git@git.hjp-consulting.com:de.persosim.simulator
+git clone git@git.hjp-consulting.com:org.globaltester.cryptoprovider
+git clone git@git.hjp-consulting.com:org.globaltester.logging
+git clone git@git.hjp-consulting.com:org.globaltester.parent
