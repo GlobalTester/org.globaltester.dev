d3af9182528bf2ab79508f440f645c88fc222748
Alexander May <alexander.may@hjp-consulting.com>
2015-08-09 23:03:25 +0200

    add PersoSim checkout script

---
+++ com.hjp.releng/checkout/persosim.bat
@@ -0,0 +1,6 @@
+git clone git@git.hjp-consulting.com:de.persosim.driver.connector
+git clone git@git.hjp-consulting.com:de.persosim.rcp
+git clone git@git.hjp-consulting.com:de.persosim.simulator
+git clone git@git.hjp-consulting.com:org.globaltester.cryptoprovider
+git clone git@git.hjp-consulting.com:org.globaltester.logging
+git clone git@git.hjp-consulting.com:org.globaltester.parent
