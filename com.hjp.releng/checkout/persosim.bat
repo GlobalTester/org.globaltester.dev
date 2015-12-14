8b85a34e6ffdd0ab35474e4bbdcab3a89d711ea3
Martin Boonk <martin.boonk@hjp-consulting.com>
2015-12-14 15:57:35 +0100

    Renamed com.hjp.releng to org.globaltester.dev

--- com.hjp.releng/checkout/persosim.bat
+++
@@ -1,7 +0,0 @@
-git clone git@git.hjp-consulting.com:de.persosim.driver.connector
-git clone git@git.hjp-consulting.com:de.persosim.rcp
-git clone git@git.hjp-consulting.com:de.persosim.simulator
-git clone git@git.hjp-consulting.com:org.globaltester.cryptoprovider
-git clone git@git.hjp-consulting.com:org.globaltester.logging
-git clone git@git.hjp-consulting.com:org.globaltester.parent
-git clone git@git.hjp-consulting.com:org.globaltester.simulator
