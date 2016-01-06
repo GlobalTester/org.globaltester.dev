75e99ab72491d2b07cf5d91c464e0217345c95e6
Martin Boonk <martin.boonk@hjp-consulting.com>
2016-01-06 12:34:59 +0100

    correct filtering for tag types

--- org.globaltester.dev.tools/releng/helper.sh
+++ org.globaltester.dev.tools/releng/helper.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 function getLastTag {
 	#$1 tag type, e.g. release
-	#$2 product, e.g. de.persosim.rcp
-	echo `git tag --list $1/$2/* --sort=version:refname | sed -e '$!d'`
+	#$2 type qualifier, e.g. de.persosim.rcp for products
+	if [ -z "$2" ]
+	then
+		FILTER="$1/*"
+	else
+		FILTER="$1/$2/*"
+	fi
+	
+	echo `git tag --list $FILTER --sort=version:refname | sed -e '$!d'`
 }
 
 function getLastTagRange {
 	#$1 tag type, e.g. release
-	#$2 product, e.g. de.persosim.rcp
+	#$2 type qualifier, e.g. de.persosim.rcp for products
 	LAST_TAG=`getLastTag $1 $2`
 	if [ -z "$LAST_TAG" ]
 	then
@@ -18,6 +25,38 @@ function getLastTagRange {
 		LAST_TAGGED_COMMIT_ID=`git rev-parse $LAST_TAG`
 		echo $LAST_TAGGED_COMMIT_ID..
 	fi
+}
+
+function getCurrentVersionFromChangeLog {
+	CHANGELOG_FILE=$1
+	while read CURRENT_LINE; do
+		VERSION=`echo $CURRENT_LINE | sed -e 's|Version \([0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}\) ([0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\})|\1|g'`
+		if [ ! -z "$VERSION" ]
+		then
+			echo $VERSION
+			break
+		fi
+	done < $CHANGELOG_FILE
+}
+
+function getChangeLogSinceVersion {
+	CHANGELOG_FILE=$1
+	VERSION_TO_SEARCH=$2
+	RESULT=`mktemp`
+	while read CURRENT_LINE; do
+		VERSION=`echo $CURRENT_LINE | sed -e 's|Version \([0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}\) ([0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\})|\1|g'`
+		if [ $VERSION == $VERSION_TO_SEARCH ]
+		then
+			break
+		else
+			if [ ! -z "$VERSION" ]
+			then
+				$CURRENT_LINE"\n" >> $RESULT
+			fi
+		fi
+	done < $CHANGELOG_FILE
+	cat $RESULT
+	rm $RESULT
 }
 
 function whereAmI {
