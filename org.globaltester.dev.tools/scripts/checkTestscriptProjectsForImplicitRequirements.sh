2d186a7ab6f5952ed42f8b016690602700b94c35
Sascha Lutters <sascha.lutters@hjp-consulting.com>
2016-02-23 15:04:04 +0100

    extracted method to separate helper script

--- org.globaltester.dev.tools/scripts/checkTestscriptProjectsForImplicitRequirements.sh
+++ org.globaltester.dev.tools/scripts/checkTestscriptProjectsForImplicitRequirements.sh
@@ -2,6 +2,7 @@
 # must be called from root directory for all repos
 set -e
 . org.globaltester.dev/org.globaltester.dev.tools/scripts/helper.sh
+. org.globaltester.dev/org.globaltester.dev.tools/scripts/projectHelper.sh
 set +e
 
 METAINFDIR='META-INF'
@@ -46,35 +47,6 @@ function findDir(){
 	done
 	
 	FINDDIRRESULT="$CURRDEPTMP"
-	
-	return 0
-}
-
-function extractFieldFromManifest() {
-	MANIFESTFILE="$1"
-	IDENTIFIER="$2"
-	
-	LINE=`grep "$IDENTIFIER" "$MANIFESTFILE" -A500 -B0`
-	GREPRESULT=$?
-	
-	if [[ $GREPRESULT != '0' ]]
-		then
-			return 1
-	fi
-	
-	CLEANEDLINE=`echo "$LINE" | sed "s|^$IDENTIFIER:\s| |; /^[^ ]/q"| sed -e "/^[^ ]/d"`
-	
-	EXTRACTFIELDRESULT=""
-	count=0
-	while read -r CURRENTREQUIREMENT
-	do
-		TMPREQ=$(echo "$CURRENTREQUIREMENT" | sed 's/^[ \t]*//;s/[ \t]*$//' | cut -d ';' -f 1 | cut -d ',' -f 1)
-		EXTRACTFIELDRESULT=`echo -e "$EXTRACTFIELDRESULT"'\n'"$TMPREQ"`
-	done <<< "$CLEANEDLINE"
-	
-	EXTRACTFIELDRESULT="$(echo -e "${EXTRACTFIELDRESULT}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e '/^$/d')"
-	
-	echo "$EXTRACTFIELDRESULT"
 	
 	return 0
 }
