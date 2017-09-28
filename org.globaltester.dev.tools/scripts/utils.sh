bd90d378f6eae78c8db9f2a2a6769661540afe1d
Martin Boonk <martin.boonk@secunet.com>
2017-09-28 14:07:49 +0200

    Adds bash-completion to eee command

--- org.globaltester.dev.tools/scripts/utils.sh
+++ org.globaltester.dev.tools/scripts/utils.sh
@@ -208,6 +208,18 @@ function eee {
 	ee "$@"
 }
 
+_eee()
+{
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+    COMPREPLY=( $(compgen -W "$( command ls ~/dev/env/ )" -- ${cur}) )
+}
+complete -F _eee eee
+
+
 function eeee {
 	eee $1
 	exit
